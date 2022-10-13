59c1c30b48ac656f9a4976cf03a2b41ad97d2ebe
ma5ter <i@mya.su>
2022-10-13 15:11:04 +0300

    correct include flag variable

--- CMakeDetermineASM_PICCompiler.cmake
+++ CMakeDetermineASM_PICCompiler.cmake
@@ -7,5 +7,7 @@ debug_position(${CMAKE_CURRENT_LIST_FILE} ${CMAKE_CURRENT_LIST_LINE})
 include(${CMAKE_CURRENT_LIST_DIR}/common.cmake)
 # find the compiler
 find_compiler(ASM_PIC XC8_ASM "xc8-cc;" "${XC8_PATH_HINTS}" "as;asm;s;S" .o YES 9)
+# add include flag
+set(CMAKE_INCLUDE_FLAG_ASM_PIC "-I")
 # create cache config
 configure_compiler(ASM_PIC)
